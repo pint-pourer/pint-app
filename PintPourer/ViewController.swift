a4c3b4f7060b5492f1a5b5a9533121eb9562832e
Aishwarya Pattar <ajp15@ic.ac.uk>
2019-01-26 16:18:32 +0000

    coded the background colour into the ViewController

--- PintPourer/ViewController.swift
+++ PintPourer/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // set background colour
+        self.view.backgroundColor = #colorLiteral(red: 0.968627451, green: 0.7254901961, blue: 0.462745098, alpha: 1)
     }
 
     // function for when Pour button is pressed
