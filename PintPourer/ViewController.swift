1c701b139c0e432b62bb0ec4b1e179f8783b30ec
Aishwarya Pattar <ajp15@ic.ac.uk>
2019-01-26 15:59:37 +0000

    initialised pour button

--- PintPourer/ViewController.swift
+++ PintPourer/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    // function for when Pour button is pressed
+    @IBAction func pourButtonTapped(_ sender: Any) {
+        
+        // tell raspberry pi to pour pint
+        
+        // animation of app screen
+        
+        // make the button unresponsive for a period of time so that clicking won't trigger an action
+        
+        
+    }
+    
 
 }
 
